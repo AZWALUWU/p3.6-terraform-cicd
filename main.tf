d2bceb59a3b5f8415ce3c7da4ede33139145d160
AZWALUWU <azwaluwu472@gmail.com>
2026-07-01 11:11:45 +0700

    feat: tambah fitur auto-comment plan ke PR dan perbaiki format

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Sengaja dibuat berantakan untuk tes terraform fmt
 resource "aws_s3_bucket" "cicd_bucket" {
-bucket = "my-terraform-cicd-test-bucket"
-tags = {
-Environment = "Dev"
-Project = "Terraform-CICD"
-}
+  bucket = "my-terraform-cicd-test-bucket"
+  tags = {
+    Environment = "Dev"
+    Project     = "Terraform-CICD"
+  }
 }
